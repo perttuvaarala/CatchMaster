2b4007a794f3aa4f319d924fe094b13ac461fb65
Henri Vuento <36409785+moikonna@users.noreply.github.com>
2023-10-05 18:50:38 +0300

    Update to map
    map centering works now

--- packages/app/src/components/Map.tsx
+++ packages/app/src/components/Map.tsx
@@ -1,35 +1,57 @@
-import { MapContainer, Marker, Popup, TileLayer} from "react-leaflet"
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 import 'leaflet/dist/leaflet.css';
 import { LatLngExpression } from "leaflet";
 import { useEffect, useState } from "react";
 
 const Map = () => {
-    const [position, setPosition] = useState<LatLngExpression>([0,0]); 
-    useEffect(() => {        
+    const [position, setPosition] = useState<LatLngExpression | null>(null); 
+    const [loading, setLoading] = useState<boolean>(true);
+
+    useEffect(() => {
         navigator.geolocation.getCurrentPosition(
-          (location) => {
-            const { latitude, longitude } = location.coords;
-            setPosition([latitude, longitude]);
-          },
-          (error) => {
-            console.error(error.message);
-          }
+            (location) => {
+                const { latitude, longitude } = location.coords;
+                setPosition([latitude, longitude]);
+            },
+            (error) => {
+                console.error(error.message);
+            }
         );
-        
-      }, []);
-    return(
-    <MapContainer center={position} zoom={13} scrollWheelZoom={false} style={{height:"30rem", width:"60rem", margin:"2rem"}}>
-    <TileLayer
-    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-    />
-    <Marker position={position}>
-    <Popup>
-      A pretty CSS3 popup. <br /> Easily customizable.
-    </Popup>
-    </Marker>
-    </MapContainer>
-  )
+
+    }, []);
+
+    
+    useEffect(() => {
+        if (position === null) {
+            setLoading(true);
+        } else {
+            setLoading(false);
+        }
+    }, [position]);
+
+    return (
+        <div>
+            {loading ? (
+                
+                <p>Loading...</p>
+            ) : position ? ( 
+                <MapContainer center={position} zoom={13} scrollWheelZoom={false} style={{ height: "30rem", width: "60rem", margin: "2rem" }}>
+                    <TileLayer
+                        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    />
+                    <Marker position={position}>
+                        <Popup>
+                            A pretty CSS3 popup. <br /> Easily customizable.
+                        </Popup>
+                    </Marker>
+                </MapContainer>
+            ) : (
+                
+                <p>Unable to obtain location data.</p>
+            )}
+        </div>
+    )
 }
 
-export default Map
+export default Map;
