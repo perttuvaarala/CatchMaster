80a28f65cc2361805faae32f3a3ea639062cd78e
adnanavni <adnan.avni@hotmail.com>
2023-10-06 12:34:14 +0300

    env in vite

--- packages/app/src/main.tsx
+++ packages/app/src/main.tsx
@@ -1,20 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import { router } from './router'
-import { RouterProvider } from 'react-router-dom'
-import { ApolloProvider } from '@apollo/client';
-import { ApolloClient, InMemoryCache } from '@apollo/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { router } from "./router";
+import { RouterProvider } from "react-router-dom";
+import { ApolloProvider } from "@apollo/client";
+import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: 'http://localhost:3000', // Replace with your Apollo Server URL
-  cache: new InMemoryCache(),
+	uri: import.meta.env.VITE_SERVER_URL,
+	cache: new InMemoryCache(),
 });
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <ApolloProvider client={client}>
-      <RouterProvider router={router} />
-    </ApolloProvider>
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")!).render(
+	<React.StrictMode>
+		<ApolloProvider client={client}>
+			<RouterProvider router={router} />
+		</ApolloProvider>
+	</React.StrictMode>,
+);
