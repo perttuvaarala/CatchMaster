96a70b80fba4ac491dd7a14c468b037a65a0dea1
perttuvaarala <perttu.vaarala@metropolia.fi>
2023-10-08 18:40:16 +0300

    vercelfix

--- packages/app/src/components/Map.tsx
+++ packages/app/src/components/Map.tsx
@@ -1,4 +1,4 @@
-import { MapContainer, Marker, Popup, TileLayer, useMap } from "react-leaflet";
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import L, { LatLngExpression } from "leaflet";
 import {  useEffect, useState } from "react";
