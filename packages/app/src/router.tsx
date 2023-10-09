fb050a8a7cd6945c1be91d546082a02f054d4611
roopeky <roope.kylli@metropolia.fi>
2023-10-09 12:31:06 +0300

    about page fix final

--- packages/app/src/router.tsx
+++ packages/app/src/router.tsx
@@ -1,37 +1,38 @@
-import {createBrowserRouter} from 'react-router-dom'
-import Root from './views/Root'
-import Main from './views/mainView'
-import Map from './views/mapView'
-import Weather from './views/weatherView'
-import Profile from './views/profileView'
-import NewPost from './views/NewPostView'
-import About from './components/About'
+import { createBrowserRouter } from "react-router-dom";
+import Root from "./views/Root";
+import Main from "./views/mainView";
+import Map from "./views/mapView";
+import Weather from "./views/weatherView";
+import Profile from "./views/profileView";
+import NewPost from "./views/NewPostView";
+import About from "./components/About";
 
-export const router = createBrowserRouter([{
-  path: '/',
-    element: <Root />,
-    children: [
-      { path: '', element: <Main /> },
-      {
-        path: 'map',
-        element: <Map />
-      },
-      {
-        path: 'weather',
-        element: <Weather />
-      },
-      {
-        path: 'profile',
-        element: <Profile />
-      },
-      {
-        path: 'NewPost',
-        element: <NewPost />
-      },
-      {
-        path: 'about',
-        element: <About />
-      },
-    ]
-  }
-])
+export const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <Root />,
+		children: [
+			{ path: "", element: <Main /> },
+			{
+				path: "map",
+				element: <Map />,
+			},
+			{
+				path: "weather",
+				element: <Weather />,
+			},
+			{
+				path: "profile",
+				element: <Profile />,
+			},
+			{
+				path: "NewPost",
+				element: <NewPost />,
+			},
+			{
+				path: "about",
+				element: <About />,
+			},
+		],
+	},
+]);
