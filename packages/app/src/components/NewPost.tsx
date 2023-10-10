8a312abcfeb838ab61f621bcb7312668942990b5
Henri Vuento <36409785+moikonna@users.noreply.github.com>
2023-10-10 21:56:05 +0300

    fixed issue caused by #43
    added functionality to add bait used in catch while making new post

--- packages/app/src/components/NewPost.tsx
+++ packages/app/src/components/NewPost.tsx
@@ -1,13 +1,46 @@
 import { ChangeEvent, FormEvent, useState } from "react";
 import { useCurrentUser } from "../hooks/useCurrentUser";
 import { CreatePostDocument,  useCreatePostMutation } from "../grapqhl/AllPosts.generated";
+import styled from "styled-components";
+import { useGetAllBaitsQuery, useUserLureBoxQuery } from "./LureBox/graphql/LureBox.generated";
+
+const StyledSelect = styled.select`
+	background-color: rgb(36 46 27);
+	border: 1px solid rgb(6, 15, 13);
+	border-radius: 0.5rem;
+	height: 3rem;
+	color: rgb(214, 232, 211);
+	padding: 0px 1rem;
+	font-size: 1rem;
+`;
+
 const NewPost: React.FC = () => {
     const [length, setLength] = useState<number | undefined>(undefined);
     const [weight, setWeight] = useState<number | undefined>(undefined);
     const [Content, setContent] = useState('');
     const [image, setImage] = useState<string | null>(null);
-    const [CreatePost] = useCreatePostMutation();
-    const currentUser = useCurrentUser();
+    const [baitID, setBaitID] = useState("");
+
+  const currentUser = useCurrentUser();
+  const [CreatePost] = useCreatePostMutation();
+  const userID = currentUser?.id;
+  const { data, loading, error } = useUserLureBoxQuery({
+		skip: !userID,
+		variables: {
+			id: userID!,
+		},
+	});
+  const {
+		data: baitData,
+		loading: baitLoading,
+		error: baitError,
+	} = useGetAllBaitsQuery();
+  if (loading || baitLoading) return <p>Loading...</p>;
+	if (error) return `Error! ${error.message}`;
+	if (baitError) return `Error! ${baitError.message}`;
+	if (!data || !baitData) return <p>Not found</p>;
+    
+    
     
     const handleLengthChange = (e: ChangeEvent<HTMLInputElement>) => {
       const value = parseFloat(e.target.value);
@@ -49,7 +82,7 @@ const NewPost: React.FC = () => {
               weight: weight|| 0,
               content: Content|| "",
               imagelink: "https://imgur.com/gallery/8aeqFkQ"|| "",
-              baitID: "651c0147b7af098d2461395b",
+              baitID: baitID||"",
               userID: currentUser?.id || "",
               fishID: "651c188c9e07b36f246ba9ee" ||""
             },
@@ -68,6 +101,7 @@ const NewPost: React.FC = () => {
           console.log('Image:', image);
           console.log('Latitude:', latitude);
           console.log('Longitude:', longitude);
+          console.log(baitID);
         } catch (e) {
           console.error('Error getting location:', e);
         }
@@ -121,8 +155,32 @@ const NewPost: React.FC = () => {
           />
           {image && <img src={image} alt="Uploaded" style={{ maxWidth: '100px' }} />}
         </div>
+        <StyledSelect
+				onChange={(e) =>
+					setBaitID(
+						e.target.selectedIndex === baitData.getAllBaits.length
+							? "newBait"
+							: baitData.getAllBaits[e.target.selectedIndex].id,
+					)
+				}
+			>
+				{baitData.getAllBaits
+					.concat({
+						id: "newBait",
+						name: "create new bait",
+						brand: "",
+						color: "",
+						weight: -1,
+					})
+					.map((bait) => (
+						<option value={bait.name} key={bait.id}>
+							{bait.name}
+						</option>
+					))}
+			</StyledSelect>
         <button type="submit">Submit</button>
       </form>
+      
     );
   };
   
